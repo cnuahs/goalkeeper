2914bd73f947699d9cc2c2fdb13a0c72fa502c83
Shaun Cloherty <s.cloherty@ieee.org>
2018-03-02 22:05:31 +1100

    Added config.js to store the slack verification token etc.

---
+++ config.js
@@ -0,0 +1,7 @@
+// GoalKeeper web app configuration script
+
+// 2018-03-02 - Shaun L. Cloherty <s.cloherty@ieee.org>
+
+function slackToken() {
+  return 'SLACK VERIFICATION TOKEN HERE';   // slack verification token
+}
