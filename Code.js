548f4ea439d976e1d6d088f191e295c82508274d
Shaun Cloherty <s.cloherty@ieee.org>
2018-03-04 15:03:31 +1100

    wip: code clean-up and added test functions.

--- Code.js
+++ Code.js
@@ -1,20 +1,22 @@
-// bare bones slash command handler for slack...
+// GoalKeeper slash command handler for slack...
 
-// POST payloads from slack slash commands contain the following parameters:
+// POST payloads from slack slash commands contain the following parameter object:
 //
-//   user_name: shaunc
-//   trigger_id: 323566020866.140237611429.d0b91a86770f6d14f4f35304f6be858c
-//   user_id: U444CRH8S
-//   team_id: T446ZHZCM
-//   response_url: https://hooks.slack.com/commands/T446ZHZCM/324617447287/H6jQI1o93lOIrQBBnvVdvAj5
-//   channel_name: zapier
-//   token: SLACK VERIFICATION TOKEN
-//   team_domain: marmolab
-//   command: /goal
-//   channel_id: C9H2SGDAB
-//   text: my new goal
+//   payload.parameter = {
+//     user_name: "~"
+//     trigger_id: "~"
+//     user_id: "Uxxxxxxxx"
+//     team_id: "Txxxxxxxx"
+//     response_url: "~"
+//     channel_name: "~"
+//     token: "~"
+//     team_domain: "~"
+//     command: "~"
+//     channel_id: Cxxxxxxxx
+//     text: ""
+//   }
 
-// 2018-03-02 - Shaun L. Cloherty <s.cloherty@ieee.org>
+// 2018-03-04 - Shaun L. Cloherty <s.cloherty@ieee.org>
 
 function doPost(payload) {
 
@@ -50,13 +52,10 @@ function goalHandler(uid,uname,args) {
   //   /goal new goal <-- set current goal to 'new goal' (shows in channel)
   //   /goal @user new goal <-- set current goal for @user (shows in channel)?
   //
+  //   /goal help
+  //   /goal connect
+  //
   // so, args should be a string like: "<@U444CRH8S|shaunc> shaun's new goal"
-
-//  // DEBUG ONLY
-//  uid = "U444CRH8S";
-//  uname = "shaunc"
-//  args = "";
-//  //
 
   args = parseArgs(args); // uid, uname, body/goal
   if (args.uid) {
@@ -88,6 +87,50 @@ function goalHandler(uid,uname,args) {
   return ContentService.createTextOutput(result);
 }
 
+function testGoalHandler() {
+  // test goalHandler()
+  var uid = "Uxxxxxxxx";
+  var uname = "nobody";  
+
+  // usage: /goal
+  var args = "";  
+  Logger.log("Testing goalHandler(%s,%s,%s)",uid,uname,args);
+  
+  var result = goalHandler(uid,uname,args);
+  
+  Logger.log("- %s.",result.getContent());
+  
+  // usage; /goal @uname
+  args = "<@" + uid + "|" + uname + "> ";  
+  Logger.log("Testing goalHandler(%s,%s,%s)",uid,uname,args);
+  
+  result = goalHandler(uid,uname,args); 
+  
+  Logger.log("- %s.",result.getContent());
+  
+  // usage: /goal lorem ipsum
+  args = "lorem ipsum";  
+  Logger.log("Testing goalHandler(%s,%s,%s)",uid,uname,args);
+  
+  result = goalHandler(uid,uname,args); 
+  
+  Logger.log("- %s.",result.getContent());
+  
+  // usage: /goal @uname lorem ipsum
+  args = "<@" + uid + "|" + uname + "> lorem ipsum";  
+  Logger.log("Testing goalHandler(%s,%s,%s)",uid,uname,args);
+  
+  result = goalHandler(uid,uname,args); 
+  
+  Logger.log("- %s.",result.getContent());
+  
+  // now the supported actions:
+  
+  // usage: /goal help
+  
+  // usage: /goal connect
+}
+
 function scoreHandler(uid,uname,args) {
   // handler for /score commands
 
@@ -96,6 +139,8 @@ function scoreHandler(uid,uname,args) {
   //   /score <-- return current users score (user only, not in channel)
   //   /score @user <-- return the score for @user (user only, not in nchannel)
   //   /score @user score <-- set the score for @user (note: cannot set your own score) (shows in channel)
+  //
+  //   /score help
 }
 
 function parseArgs(args) {
@@ -131,39 +176,8 @@ function parseArgs(args) {
   return ({uid: uid, uname: uname, body: body});
 }
 
-//function getUser(uid,uname) {
-//  // get backend settings for the user
-//
-//  var ss = SpreadsheetApp.openById(sheetId());
-//  SpreadsheetApp.setActiveSpreadsheet(ss); // handy...?
-//
-//  //  var ss = SpreadsheetApp.getActiveSpreadsheet();
-//
-//  // look for the users sheet...
-//  hs = ss.getSheetByName(uid);
-//  if (hs == null) {
-//    hs = addUser(ss,uid,uname);
-//  }
-//
-//  // get users row on the score card
-//
-//
-//
-//  Logger.log(uname + " --> " + hs.getName() + "(" + hs.getIndex() + ")");
-//
-//
-//}
-
 function addUser(ss,uid,uname) { // OK
   // add a new user and return backend settings
-  
-//  // DEBUG ONLY
-//  ss = SpreadsheetApp.openById(sheetId());
-////  uid = "Uxxxxxxxx";
-////  uname = "nobody";
-//  uid = "U444CRH8S";
-//  uname = "shaunc";
-//  //
   
   // 1. find candidate entry on the score sheet
   var s = ss.getSheetByName("Sheet1");
@@ -176,7 +190,7 @@ function addUser(ss,uid,uname) { // OK
   }
   
   // 2. user is unknown... but use some huristics here to see if we have any close matches
-  Logger.log("User " + uid + " (" + uname + ") unknown.");
+  Logger.log("User %s (%s) is unknown.",uid,uname);
   
   // loop over known writers and see if any "match" the supplied slack user name
   var col = getColumnByName(s,["Writer"]);
@@ -201,11 +215,10 @@ function addUser(ss,uid,uname) { // OK
       // 2. if so, appropriate the existing record by assigning the current uid to this writer, 
       var uid_ = getRow(s,row,["Slack UID"])[0];
       
-      Logger.log("User " + uid + " (" + uname + ") might be " + writer + " (uid:" + uid_ + ").");
-      
+      Logger.log("User %s (%s) might be %s (uid:%s).",uid,uname,writer,uid_);
       
       if (uid_.length === 0) {
-        Logger.log("Ok. Appropriating " + writer + " to be " + uid + " (" + uname + ").");
+        Logger.log("Ok. Appropriating %s to be %s (%s).",writer,uid,uname);
         setRow(s,row,["Slack UID"],[uid]);
         break writerloop;
       }
@@ -240,68 +253,63 @@ function addUser(ss,uid,uname) { // OK
   return row;
 }
 
-function getCurrentGoal(uid) {  
+function testAddUser() {
+  // test addUser()
+  var ss = SpreadsheetApp.openById(sheetId());
+  uid = "Uxxxxxxxx";
+  uname = "nobody";  
+  
+  Logger.log("Testing addUser(%s,%s,%s)",ss.getName(),uid,uname);
+  
+  var result = addUser(ss,uid,uname);
+  
+  Logger.log("- %s.",result);
+}
+
+function setCurrentGoal(uid,uname,goal) {
   var ss = SpreadsheetApp.openById(sheetId());
   SpreadsheetApp.setActiveSpreadsheet(ss); // handy...?
 
-//  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var s = ss.getSheetByName("Sheet1"); // FIXME
+  
+  var row = getRowByColumn(s,["Slack UID"],[uid])[0];
+  
+  if (row == null) {
+    var row = addUser(ss,uid,uname);
+  }
+  
+  setRow(s,row,["Goal"],[goal]);
+  
+  // update the history sheet
+  var h = ss.getSheetByName(uid);
+  h.insertRowAfter(h.getLastRow()); // append row at the bottom (inherits formatting)
+  setRow(h,h.getLastRow()+1,["Date","Goal"],[new Date(),goal]);
+}
 
-//  // DEBUG ONLY
-//  uid = "U444CRH8S";
-//  //
+function testSetCurrentGoal() {
+  // test setCurrentGoal()
+  var ss = SpreadsheetApp.openById(sheetId());
+  s = ss.getSheetByName("Sheet1");
+  
+  var uname = "nobody";
+  
+  var row = getRowByColumn(s,["Writer"],[uname]);
+  var uid = getRow(s,row,["Slack UID"]);
+  
+  var goal = "lorem ipsum";
+  
+  Logger.log("Testing setCurrentGoal(%s,%s,%s)",uid,uname,goal);
+  
+  var result = setCurrentGoal(uid,uname,goal);
+  
+  Logger.log("- %s",result);
+}
+
+function getCurrentGoal(uid) {  
+  var ss = SpreadsheetApp.openById(sheetId());
+  SpreadsheetApp.setActiveSpreadsheet(ss); // handy...?
   
   var s = ss.getSheetByName("Sheet1"); // FIXME
-//  var range = s.getDataRange();
-//
-//  var nRows = range.getHeight();
-//  var nCols = range.getWidth();
-//
-//  var pat = [/writer.*/i,/goal.*/i]; // regexp for column headings
-//
-//  // first nonempty row contains column headings...
-//  rowloop:
-//  for (var row = 1; row <= nRows; row++) { // getHeight()
-//    for (var col = 1; col <= nCols; col++) { // getWidth()
-//      var value = range.getCell(row,col).getDisplayValue();
-//      if (value) {
-//        // (row,col) is the first non-empty cell
-//        var headings = s.getRange(row,col,1,nCols).getValues()[0];
-//
-//        // look for our column headings... pat
-//        var colIdx = pat.map(function(re) {
-//          for (var i = 0; i < headings.length; i++) {
-//            if (re.test(headings[i])) {
-//              return i + 1; // rows start at 1
-//            }
-//          }
-//        });
-//
-//        // update range
-//        range = s.getRange(row+1,col,nRows-row,nCols-col+1); // note: excludes headings...
-//
-////        Logger.log(colIdx);
-//        break rowloop;
-//      }
-//    }
-//  }
-//
-//  nRows = range.getHeight();
-//  nCols = range.getWidth();
-//
-//  // get list of writers
-//  writerloop:
-//  for (var row = 1; row <= nRows; row++) { // getHeight()
-//    var writer = range.getCell(row,colIdx[0]).getDisplayValue(); // colIdx[0] is the writer column
-//
-////    Logger.log(writer);
-//
-//    var re = new RegExp(".*"+writer+".*","i");
-//    if (re.test(uname)) {
-//      var goal = range.getCell(row,colIdx[1]).getDisplayValue(); // colIdx[1] is the goal column
-//
-//      Logger.log(uname + " --> " + goal);
-//    }
-//  }
 
   var row = getRowByColumn(s,["Slack UID"],[uid]);
   
@@ -310,21 +318,27 @@ function getCurrentGoal(uid) {
   }
 }
 
+function testGetCurrentGoal() {
+  // test getCurrentGoal()
+  var ss = SpreadsheetApp.openById(sheetId());
+  s = ss.getSheetByName("Sheet1");
+  
+  var row = getRowByColumn(s,["Writer"],["Shaun"]);
+  var uid = getRow(s,row,["Slack UID"]);
+  
+  Logger.log("Testing getCurrentGoal(%s)",uid);
+  
+  var result = getCurrentGoal(uid);
+  
+  Logger.log("- %s",result);
+}
+
 function getColumnByName(s,name) { // OK
   // find column(s) by name, returning column number(s)
-
-//  // DEBUG ONLY
-//  var ss = SpreadsheetApp.openById(sheetId());
-//  s = ss.getSheetByName("Sheet1");
-//  name = ["Writer"];
-//  //
-
   var range = s.getDataRange();
 
   var nRows = range.getHeight();
   var nCols = range.getWidth();
-
-//  var pat = [/writer.*/i,/goal.*/i]; // regexp for column headings
 
   // first nonempty row contains column headings...
   rowloop:
@@ -334,10 +348,6 @@ function getColumnByName(s,name) { // OK
       if (value) {
         // first non-empty cell
         var headings = s.getRange(row,col,1,nCols-col+1).getValues()[0];
-
-//        if (typeof(name) != "Array") {
-//          name = [name]; // for easy looping
-//        }
 
         name = name.map(function(n) {
           if (typeof(n) == "RegExp") {
@@ -360,28 +370,25 @@ function getColumnByName(s,name) { // OK
       }
     } // col
   } // row
-
-//  if (idx.length == 1) {
-//    idx = idx[0];
-//    
-//    if (typeof(idx) == "undefined") {
-//      idx = null; // column not found
-//    }
-//  }
   
   return idx
 }
 
+function testGetColumnByName() {
+  // test getColumnByName()
+  var ss = SpreadsheetApp.openById(sheetId());
+  s = ss.getSheetByName("Sheet1");
+  name = ["Writer"];
+  
+  Logger.log("Testing getColumnByName(%s,%s)",s.getName(),name);
+  
+  var result = getColumnByName(s,name);
+  
+  Logger.log("- %s",result);
+}
+
 function getRowByColumn(s,name,value) { // OK
   // find row by contents of column name, returning row number
-
-//  // DEBUG ONLY
-//  var ss = SpreadsheetApp.openById(sheetId());
-//  s = ss.getSheetByName("Sheet1");
-//  name = ["Writer"];
-//  value = ["Shaun"];
-//  //
-  
   var range = s.getDataRange();
 
   var nRows = range.getHeight();
@@ -392,10 +399,6 @@ function getRowByColumn(s,name,value) { // OK
   if (col == null) {
     return null; // column not found?
   }
-
-//  if (typeof(value) != "Array") {
-//    value = [value]; // for easy looping
-//  }
 
   var idx = new Array();
   for (var row = 1; row <= nRows; row++) {
@@ -408,28 +411,27 @@ function getRowByColumn(s,name,value) { // OK
     });
     
   }
-  
-//  if (idx.length == 1) {
-//    idx = idx[0];
-//    
-//    if (typeof(idx) == "undefined") {
-//      idx = null;
-//    }   
-//  }
-  
+    
   return idx;
 }
 
+function testGetRowByColumn() {
+  // test getRowByColumn()
+  var ss = SpreadsheetApp.openById(sheetId());
+  s = ss.getSheetByName("Sheet1");
+  name = ["Writer"];
+  value = ["Shaun"];
+  
+  Logger.log("Testing getRowByColumn(%s,%s,%s)",s.getName(),name,value);
+  
+  var result = getRowByColumn(s,name,value);
+  
+  Logger.log("- %s",result);
+}
+
+
 function setRow(s,row,name,value) { // SET name = value WHERE row; OK
   // set column name(s) to value(s) on row
-  
-//  // DEBUG ONLY
-//  var ss = SpreadsheetApp.openById(sheetId());
-//  s = ss.getSheetByName("Sheet1");
-//  row = getRowByColumn(s,["Writer"],["Shaun"]);
-//  name = ["Goal"];
-//  value = ["A newish goal for Shaun."];
-//  //
   
 //  var range = s.getDataRange();
   var range = s.getRange(1,1,row,s.getLastColumn()); // in case row is empty and therefore not included by getDataRange()
@@ -444,45 +446,45 @@ function setRow(s,row,name,value) { // SET name = value WHERE row; OK
   }  
 }
 
+function testSetRow() {
+  // test setRow()  
+  var ss = SpreadsheetApp.openById(sheetId());
+  s = ss.getSheetByName("Sheet1");
+  row = getRowByColumn(s,["Writer"],["Shaun"]);
+  name = ["Goal"];
+  value = ["A new goal for Shaun."];
+    
+  Logger.log("Testing setRow(%s,%s,%s,%s)",s.getName(),row,name,value);
+  
+  var result = setRow(s,row,name,value);
+  
+  Logger.log("- %s",result);
+}
+
 function getRow(s,row,name) { // SELECT name WHERE row; OK
   // get value(s) from column name(s) on row
-  
-//  //  // DEBUG ONLY
-//  var ss = SpreadsheetApp.openById(sheetId());
-//  s = ss.getSheetByName("Sheet1");
-//  row = getRowByColumn(s,["Writer"],["Shaun"]);
-//  name = ["Writer","Goal"];
-//  //
-  
+    
   var range = s.getDataRange();
   
   var value = getColumnByName(s,name).map(function (col) {
     return range.getCell(row,col).getValue();
   });
-  
-//  Logger.log(value);
-  
+    
   return value;
 }
 
-function setCurrentGoal(uid,uname,goal) {
+function testGetRow() {
+  // test getRow()  
   var ss = SpreadsheetApp.openById(sheetId());
-  SpreadsheetApp.setActiveSpreadsheet(ss); // handy...?
-
-  var s = ss.getSheetByName("Sheet1"); // FIXME
+  s = ss.getSheetByName("Sheet1");
+  row = getRowByColumn(s,["Writer"],["Shaun"]);
+  name = ["Writer","Goal"];
   
-  var row = getRowByColumn(s,["Slack UID"],[uid])[0];
+  Logger.log("Testing getRow(%s,%s,%s)",s.getName(),row,name);
   
-  if (row == null) {
-    var row = addUser(ss,uid,uname);
-  }
+  var result = getRow(s,row,name);
   
-  setRow(s,row,["Goal"],[goal]);
-  
-  // update the history sheet
-  var h = ss.getSheetByName(uid);
-  h.insertRowAfter(h.getLastRow()); // append row at the bottom (inherits formatting)
-  setRow(h,h.getLastRow()+1,["Date","Goal"],[new Date(),goal]);
+  Logger.log("- %s",result);
 }
 
 function mkErrorAttachment(msg) {
