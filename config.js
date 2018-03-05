1711b4db923b0dccb6b684112a63fffa90e1bfeb
Shaun Cloherty <s.cloherty@ieee.org>
2018-03-05 22:37:56 +1100

    Updated config template.

--- config.js
+++ config.js
@@ -2,18 +2,18 @@
 
 // 2018-03-02 - Shaun L. Cloherty <s.cloherty@ieee.org>
 
-function slackToken() {
-  return "";   // slack verification token
+function slackToken() { // (required) slack verification token
+  return "";
 }
 
-function sheetId() {
-  return ""; // google sheet id (testing)
+function sheetId() { // (required) google sheet id
+  return "";
 }
 
-function feedbackUid() {
-  return ""; // slack uid for feedback
+function feedbackUid() { // (optional) slack uid for feedback
+  return "";
 }
 
-function webhookUrl() {
-  return null; // [optional] slack webhook
+function webhookUrl() { // (optional) slack webhook
+  return null;
 }
